d4cccad2bb1dfe4eb4c27b57af2f93cff526ed8e
Nemo <arlenmirzak@gmail.com>
2019-01-18 14:44:30 -0800

    again

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const router = require('./wear_comRouter');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.use('/wearit.club', router);
+app.use('/', router);
 
 app.listen(8080, 'wearit.club');
 console.log('now listening on port 8080');
